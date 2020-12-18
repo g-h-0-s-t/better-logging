3ae8516d41273bccd63e4dbd61f1fe962e4be9bc
sol <g-h-0-s-t@protonmail.com>
2020-12-18 04:03:56 +0000

    removed [] from STAMP

--- src/lib/formatMessage.ts
+++ src/lib/formatMessage.ts
@@ -1,47 +1,87 @@
-import safeStringify from 'fast-safe-stringify';
-import { Color } from './types/color';
-import { Config } from './config';
-import { LogType } from './types/logType';
-import { MessageConstructionStrategy } from './enums/messageConstructionStrategy';
+import safeStringify from "fast-safe-stringify";
+import { Config } from "./config";
+import { MessageConstructionStrategy } from "./enums/messageConstructionStrategy";
+import { Color } from "./types/color";
+import { LogType } from "./types/logType";
 
-const constructFormattingContext = (logType: LogType, config: Config, message: string) => {
+const constructFormattingContext = (
+  logType: LogType,
+  config: Config,
+  message: string
+) => {
   const typeColor = config.color.type[logType];
-  const STAMP = (innerContent: string, innerColor: Color = config.color.base) => config.color.base(`[${innerColor(innerContent)}]`);
-  return ({
+  const STAMP = (innerContent: string, innerColor: Color = config.color.base) =>
+    config.color.base(`${innerColor(innerContent)}`);
+  return {
     msg: message,
     type: STAMP(logType, typeColor),
-    time24: STAMP(new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false })),
-    time12: STAMP(new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true })),
-    date: STAMP(new Date().toLocaleString('en-UK', { year: 'numeric', month: 'numeric', day: 'numeric' })),
-    unix: STAMP('' + new Date().valueOf()),
-    STAMP: (content: string | number, color?: Color) => STAMP(`${content}`, color),
-  });
+    time24: STAMP(
+      new Date().toLocaleString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: false,
+      })
+    ),
+    time12: STAMP(
+      new Date().toLocaleString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true,
+      })
+    ),
+    date: STAMP(
+      new Date().toLocaleString("en-UK", {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+      })
+    ),
+    unix: STAMP("" + new Date().valueOf()),
+    STAMP: (content: string | number, color?: Color) =>
+      STAMP(`${content}`, color),
+  };
 };
 
-const constructMessage = (strategy: MessageConstructionStrategy, args: unknown[]): [string, unknown[]] => {
-  const processArgument =  (arg: unknown): string => {
-    if (typeof arg === 'object') {
+const constructMessage = (
+  strategy: MessageConstructionStrategy,
+  args: unknown[]
+): [string, unknown[]] => {
+  const processArgument = (arg: unknown): string => {
+    if (typeof arg === "object") {
       return safeStringify(arg);
     }
     return `${arg}`;
   };
 
   if (strategy === MessageConstructionStrategy.NONE) {
-    return ['', args];
+    return ["", args];
   }
   if (strategy === MessageConstructionStrategy.FIRST) {
-    const [ first, ...rest ] = args;
+    const [first, ...rest] = args;
     return [processArgument(first), rest];
   }
   if (strategy === MessageConstructionStrategy.ALL) {
-    return [args.map(processArgument).join(' '), []];
+    return [args.map(processArgument).join(" "), []];
   }
   throw new Error(`Unknown MessageConstructionStrategy: ${strategy}`);
 };
 
-export const formatMessage = (logType: LogType, config: Config, args: unknown[]): [string, unknown[]] => {
-  const [rawMessage, remainingArgs] = constructMessage(config.messageConstructionStrategy, args);
-  const formattingContext = constructFormattingContext(logType, config, rawMessage);
+export const formatMessage = (
+  logType: LogType,
+  config: Config,
+  args: unknown[]
+): [string, unknown[]] => {
+  const [rawMessage, remainingArgs] = constructMessage(
+    config.messageConstructionStrategy,
+    args
+  );
+  const formattingContext = constructFormattingContext(
+    logType,
+    config,
+    rawMessage
+  );
   const formattedMessage = config.format(formattingContext);
   return [formattedMessage, remainingArgs];
-}
+};
